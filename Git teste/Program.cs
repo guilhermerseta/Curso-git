265171ad51e750427b755f7c27ba0f8c31efbcf3
Guilherme Seta <guilhermerseta@hotmail.com>
2020-07-01 09:53:41 -0300

    Bom dia

--- Git teste/Program.cs
+++ Git teste/Program.cs
@@ -7,7 +7,9 @@ namespace Git_teste
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom dia!");
             Console.ReadLine();
+
         }
     }
 }
